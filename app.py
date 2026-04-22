9e423c416663820b601616ffe5b2015852ce3034
Nick <n.hunter@hhrltd.com>
2026-04-22 07:34:30 +0100

    Revert "Fix sidebar scroll, add email_compose route"
    This reverts commit 2b50562e9efe10f4809e2068ea982a456dd527b6.

--- app.py
+++ app.py
@@ -894,16 +894,6 @@ def email_builder():
 
 @app.route('/email/rich')
 @login_required
-def email_compose():
-    category_filter = request.args.get('category', '')
-    if category_filter:
-        all_contacts = Contact.query.filter_by(category=category_filter).all()
-    else:
-        all_contacts = Contact.query.all()
-    contacts_data = [{'id': c.id, 'first_name': c.first_name, 'last_name': c.last_name, 'email': c.email, 'company': c.company} for c in all_contacts]
-    return render_template('email_compose.html', all_contacts=contacts_data, category_filter=category_filter)
-
-
 def rich_email():
     category_filter = request.args.get('category', '')
     if category_filter:
